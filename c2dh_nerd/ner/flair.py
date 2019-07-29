44b2b76ae02c0095c0accb5d013d5dadb5f8578b
theorm <theorm@gmail.com>
2019-07-29 09:19:26 +0200

    Extra `return_sentences` flag on FlairNER to indicate that a list of sentences should be returned in the response instead of text.

--- c2dh_nerd/ner/flair.py
+++ c2dh_nerd/ner/flair.py
@@ -7,31 +7,42 @@ from .result import NerResult, NerResultEntity
 
 flatten = lambda l: [item for sublist in l for item in sublist]
 
-def as_ner_result_entity(span, offset: int) -> NerResultEntity:
+def as_ner_result_entity(span, offset: int, index: int, return_sentences = False) -> NerResultEntity:
   # print('S', span, dir(span), span.__dict__)
   # {'tokens': [Token: 5 Robert, Token: 6 Goebbels,], 'tag': 'PER', 'score': 0.8921127617359161, 'start_pos': 17, 'end_pos': 33}
+  left = span.start_pos if return_sentences else span.start_pos + offset
+  right = span.end_pos if return_sentences else span.end_pos + offset
+  sentence_index = index if return_sentences else None
+
   return NerResultEntity(
     entity = span.text,
     tag = span.tag,
-    left = offset + span.start_pos,
-    right = offset + span.end_pos,
-    score = span.score
+    left = left,
+    right = right,
+    score = span.score,
+    sentence_index = sentence_index
   )
 
 class FlairNer(NER):
   def __init__(self, model_name='ner-fast'):
     self._tagger = SequenceTagger.load(model_name)
 
-  async def extract(self, text: TextOrSentences) -> NerResult:
+  async def extract(self, text: TextOrSentences, return_sentences = False) -> NerResult:
     sentences_and_offsets = text_to_sentences(text)
-    entities = self._extract_sentences(sentences_and_offsets)
-    return NerResult(sentences_to_text(text), entities)
+    entities = self._extract_sentences(sentences_and_offsets, return_sentences)
 
-  def _extract_sentences(self, sentence_and_offset_list: List[Tuple[str, int]]):
+    if return_sentences:
+      result_text = [s for s, _ in sentences_and_offsets]
+    else:
+      result_text = sentences_to_text(text)
+
+    return NerResult(result_text, entities)
+
+  def _extract_sentences(self, sentence_and_offset_list: List[Tuple[str, int]], return_sentences = False):
     flair_sentences = [Sentence(sentence) for sentence, _ in sentence_and_offset_list]
     self._tagger.predict(flair_sentences)
 
     return flatten([
-      [as_ner_result_entity(s, offset) for s in flair_sentence.get_spans('ner')]
-      for flair_sentence, (_, offset) in zip(flair_sentences, sentence_and_offset_list)
+      [as_ner_result_entity(s, offset, index, return_sentences) for s in flair_sentence.get_spans('ner')]
+      for index, (flair_sentence, (_, offset)) in enumerate(zip(flair_sentences, sentence_and_offset_list))
     ])
--- c2dh_nerd/ner/result.py
+++ c2dh_nerd/ner/result.py
@@ -8,18 +8,21 @@ ENTITY_TYPES = [
   'UNK'
 ]
 
+TextOrSentences = ('TextOrSentences', str, List[str])
+
 class NerResultEntity:
-  def __init__(self, entity: str, tag: str, left: int, right: int, score: float):
+  def __init__(self, entity: str, tag: str, left: int, right: int, score: float, sentence_index: int = None):
     self.entity = entity
     self.tag = tag
     self.left = left
     self.right = right
     self.score = score
+    self.sentence_index = sentence_index
 
     assert left < right, 'Left ({}) must be less than right ({})'.format(left, right)
 
 
 class NerResult:
-  def __init__(self, text: str, entities: List[NerResultEntity]):
+  def __init__(self, text: TextOrSentences, entities: List[NerResultEntity]):
     self.text = text
     self.entities = entities
